f81e2250703ab701e9f0da7b63896920da083633
shibenli <shibenli@vcredit.com>
2017-06-21 09:41:36 +0800

    1、导入kotlin支持； 2、添加demo实例module；

---
+++ app/src/main/kotlin/com/shibenli/basekot/kot/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.shibenli.basekot.kot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.shibenli.basekot.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
