464aefc478e443573de3ab59fefa509966e4ff89
shibenli <shibenli@vcredit.com>
2017-07-19 14:52:14 +0800

    测试自动更新，很稳定

--- app/src/main/java/com/shibenli/basekot/kot/MainActivity.kt
+++ app/src/main/java/com/shibenli/basekot/kot/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView.text = "hello bugfix"
+        textView.text = "hello bugfix lalala"
         titleBar.setMiddleTitleText(getString(R.string.app_name))
     }
 }
